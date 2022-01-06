3c9b0dabfa3ad60698de4aae10adc383b7b6644b
Vennotius <mcadendorff@gmail.com>
2022-01-06 15:56:23 +0200

    More tests. Invalid CipherTextException. Refactoring

--- Krypt2Library/Gusto/GustoAlphabetManager.cs
+++ Krypt2Library/Gusto/GustoAlphabetManager.cs
@@ -23,29 +23,16 @@ namespace Krypt2Library
                     throw new Exception("Invalid CryptType.");
             }
         }
-        private static void ExtendAlphabetForDecyption(string message, out List<object> added, out List<object> alphabetAsList)
+        private static void ExtendAlphabetForEncyption(string message, out List<object> addedAsList, out List<object> alphabetAsList)
         {
-            added = new List<object>();
-            alphabetAsList = StringToListOfObjects(_standardAlphabet);
-            var messageAsList = StringToListOfObjects(message);
+            InitializeForVariablesForExtendAlphabet(message, out addedAsList, out alphabetAsList, out List<object> messageAsList);
+            
+            ExtractAdditionalCharactersFromMessage(addedAsList, alphabetAsList, messageAsList);
 
-            foreach (var textElement in messageAsList)
-            {
-                if (added.Contains(textElement) == true) break;
-
-                if (alphabetAsList.Contains(textElement) == false)
-                {
-                    added.Add(textElement);
-                    alphabetAsList.Add(textElement);
-                }
-            }
+            AppendAddedToAlphabet(addedAsList, alphabetAsList);
         }
-        private static void ExtendAlphabetForEncyption(string message, out List<object> added, out List<object> alphabetAsList)
+        private static void ExtractAdditionalCharactersFromMessage(List<object> added, List<object> alphabetAsList, List<object> messageAsList)
         {
-            added = new List<object>();
-            alphabetAsList = StringToListOfObjects(_standardAlphabet);
-            var messageAsList = StringToListOfObjects(message);
-
             foreach (var textElement in messageAsList)
             {
                 if (added.Contains(textElement) == false)
@@ -56,8 +43,10 @@ namespace Krypt2Library
                     }
                 }
             }
-
-            added.Sort();
+        }
+        private static void AppendAddedToAlphabet(List<object> added, List<object> alphabetAsList)
+        {
+            added.Sort();  // For security reasons.
             foreach (var item in added)
             {
                 alphabetAsList.Add(item);
@@ -65,6 +54,43 @@ namespace Krypt2Library
         }
 
 
+        private static void ExtendAlphabetForDecyption(string message, out List<object> added, out List<object> alphabetAsList)
+        {
+            InitializeForVariablesForExtendAlphabet(message, out added, out alphabetAsList, out List<object> messageAsList);
+            
+            ExtractAdditionalCharactersFromCipherText(added, alphabetAsList, messageAsList);
+
+            CheckForInvalidCipherText(messageAsList, alphabetAsList, added);
+        }
+        private static void ExtractAdditionalCharactersFromCipherText(List<object> added, List<object> alphabetAsList, List<object> messageAsList)
+        {
+            foreach (var textElement in messageAsList)
+            {
+                if (alphabetAsList.Contains(textElement) == true) break;
+
+                added.Add(textElement);
+                alphabetAsList.Add(textElement);
+            }
+        }
+        private static void CheckForInvalidCipherText(List<object> messageAsList, List<object> alphabetAsList, List<object> added)
+        {
+            // If, after the initial added characters in the CipherText, unknown characters are found, the CipherText is invalid.
+            for (int i = added.Count; i < messageAsList.Count; i++)
+            {
+                if (alphabetAsList.Contains(messageAsList[i]) == false && added.Contains(messageAsList[i]) == false)
+                {
+                    throw new InvalidCipherException("Invalid CipherText");
+                }
+            }
+        }
+
+
+        private static void InitializeForVariablesForExtendAlphabet(string message, out List<object> added, out List<object> alphabetAsList, out List<object> messageAsList)
+        {
+            added = new List<object>();
+            alphabetAsList = StringToListOfObjects(_standardAlphabet);
+            messageAsList = StringToListOfObjects(message);
+        }
         internal static List<object> StringToListOfObjects(string input)
         {
             var output = new List<object>();
